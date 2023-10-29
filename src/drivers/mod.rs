a94bc52b4640e2529d5285c84ef19077103caa68
ajh123 <ajh123@users.noreply.github.com>
2023-10-29 17:26:10 +0000

    Tests simplify + intergration testing

--- src/drivers/mod.rs
+++ src/drivers/mod.rs
@@ -1,3 +1,2 @@
-pub mod qemu;
 pub mod serial;
 pub mod vga_buffer;
--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@
 #![feature(custom_test_frameworks)]
 // Specify that the `test_runner` function should be used to run
 // tests.
-#![test_runner(crate::test_runner)]
+#![test_runner(gtmos::test_runner)]
 // replace the generated test main with our own
 #![reexport_test_harness_main = "test_main"]
 
@@ -37,41 +37,8 @@ fn panic(info: &PanicInfo) -> ! {
     loop {}
 }
 
-// Testing implementation
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T: Fn(),
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[ok]");
-    }
-}
-
-// our panic handler in test mode
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    use crate::drivers::qemu;
-    serial_println!("[failed]\n");
-    serial_println!("Error: {}\n", info);
-    qemu::exit_qemu(qemu::QemuExitCode::Failed);
-    loop {}
-}
-
-#[cfg(test)]
-
-pub fn test_runner(tests: &[&dyn Testable]) {
-    use crate::drivers::qemu;
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run(); // new
-    }
-    qemu::exit_qemu(qemu::QemuExitCode::Success);
+    gtmos::test_panic_handler(info)
 }
